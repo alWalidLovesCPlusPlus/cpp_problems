2246da3d1a1d243c3b6b32ed065517d19e8bb19c
alWalidLovesCPlusPlus <ilovecodingxyz@gmail.com>
2024-06-22 01:09:49 -0700

    code
    code

---
+++ pascelTriangle/pascelTriangle.cpp
@@ -0,0 +1,11 @@
+// question : pascel triangle
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int row,column;
+    cin>>row>>column;
+    vector<vector<int>> vect;
+    return 0;
+}
